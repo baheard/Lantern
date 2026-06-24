161fc6c76a2cb0a92afa665317c9d6823f2eaeb5
Ben Heard <baheard@gmail.com>
2026-06-24 18:24:27 -0400

    v1.5.645: subtler darker glow, blue feature names in help dialog

--- docs/js/config.js
+++ docs/js/config.js
@@ -7,7 +7,7 @@ export const APP_CONFIG = {
   // App identity
   name: 'Lantern',
   displayName: 'Lantern',
-  version: '1.5.644',
+  version: '1.5.645',
 
   // Storage prefixes (used in localStorage keys)
   storagePrefix: 'lantern',
