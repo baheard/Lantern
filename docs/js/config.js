41f30c34610b6c2837d0a0ad818072876e09a3d8
Ben Heard <baheard@gmail.com>
2026-06-24 18:29:57 -0400

    v1.5.648: drop blue from help dialog; white text, gray card border

--- docs/js/config.js
+++ docs/js/config.js
@@ -7,7 +7,7 @@ export const APP_CONFIG = {
   // App identity
   name: 'Lantern',
   displayName: 'Lantern',
-  version: '1.5.647',
+  version: '1.5.648',
 
   // Storage prefixes (used in localStorage keys)
   storagePrefix: 'lantern',
